8c798c7dd5c412207e4dd9ff016a6f484b90973d
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-11-04 18:40:39 +0100

    Exercice terminé

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.green').mouseover(function() {
+    $('#text').css('color', 'green');
+  });
+  $('.red').mouseover(function() {
+    $('#text').css('color', 'red');
+  });
+  $('.blue').mouseover(function() {
+    $('#text').css('color', 'blue');
+  });
+  $('div').mouseout(function() {
+    $('#text').css('color', 'black');
+  });
+});
